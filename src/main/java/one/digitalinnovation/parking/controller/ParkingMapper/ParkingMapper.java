3caf9eb4cab95ca3f22dc643ca1f7044834c6c23
luanasoarezs <soaresluanagabriela@gmail.com>
2022-11-17 15:09:52 -0400

    Adicionando o Swagger

--- src/main/java/one/digitalinnovation/parking/controller/ParkingMapper/ParkingMapper.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingMapper/ParkingMapper.java
@@ -1,12 +1,12 @@
 package one.digitalinnovation.parking.controller.ParkingMapper;
 
+import one.digitalinnovation.parking.controller.DTO.ParkingCreateDTO;
 import one.digitalinnovation.parking.controller.DTO.ParkingDTO;
 import one.digitalinnovation.parking.model.Parking;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Component
@@ -14,10 +14,20 @@ public class ParkingMapper {
 
     private static final ModelMapper MODEL_MAPPER = new ModelMapper();
 
-    public ParkingDTO parkingDTO(Parking parking) {
+    public ParkingDTO toParkingDTO(Parking parking) {
         return MODEL_MAPPER.map(parking, ParkingDTO.class);
     }
     public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
-        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+        return parkingList.stream().map(this::toParkingDTO).collect(Collectors.toList());
+    }
+
+
+    public Parking toParking(ParkingDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+
+    public Parking toParkingCreate(ParkingCreateDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+
     }
 }
--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +17,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id, "DMS-11111", "SC", "CELTA", "PRETO");
+        Parking parking1 = new Parking(id1,"JKS-22222","SP","CORSA","VERMELHO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll() {
@@ -29,4 +33,15 @@ public class ParkingService {
     }
 
 
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(getUUID());
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
 }
